35082b14474f24d60d9bc38f29f8e5d393bfe8cf
Isha Jain <16226926-IshaJain11@users.noreply.replit.com>
2024-04-02 09:45:48 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,153 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
+public class Main {
+   public static void main(String[] args) throws IOException {
+       String[][] arr = {
+               {"        ####"},
+               {"########  ##"},
+               {"#          ###"},
+               {"# @$$ ##   ..#"},
+               {"# $$   ##  ..#"},
+               {"#         ####"},
+               {"###########   "}};
+     /*
+     #   (hash)      Wall
+     .   (period)    Empty goal
+     @   (at)        Player on floor
+     +   (plus)      Player on goal
+     $   (dollar)    Box on floor
+     *   (asterisk)  Box on goal
+     */
+
+     String easy1 = 
+       "5         \n" +
+       "##########\n" + 
+       "#        #\n" + 
+       "#  $  +  #\n" + 
+       "#        #\n" + 
+       "##########";
+
+
+     String easy2 = 
+        "1         \n" +
+        "#####\n" + 
+        "#.$@#\n" + 
+        "#####";
+
+     
+
+
+     String[] split = easy1.split("\n");
+     String [][] secondSplit = new String[split.length][split[1].length()];
+     for(int i = 0; i<split.length; i++) {
+       secondSplit[i] = split[i].split("");
+     }
+     String[][] first = removeLine(secondSplit);
+
+     int playerRow = getPlayer(first,"row");
+     int playerCol = getPlayer(first,"col");
+
+     State start = new State(null, first, Integer.parseInt(secondSplit[0][0]), playerRow, playerCol);
+     System.out.println("Player Coordinates: " + start.pR + ", " + start.pC);
+      System.out.println("--");
+ start.print(start.board);
+      System.out.println("--");
+
+
+
+     while(!(start.isGoal())) {
+        Scanner keyboard = new Scanner(System.in);
+        String ans = keyboard.nextLine();
+        start.applyMove(ans);
+        System.out.println("\nPlayer coordinates: " + 
+                          start.pR + ", " + start.pC);
+        System.out.println("--");
+        start.print(start.board);
+        System.out.println("--");
+     }
+     System.out.println("\nLevel finished, with " + start.counter + 
+                        isPlural(start.counter, "move") + "! Least moves needed" + isPlural(start.stepsNeeded, "is") + start.stepsNeeded + isPlural(start.stepsNeeded, "move") + ".");
+     
+
+   }
+public static String isPlural(int num, String str) {
+  if(str.equals("is")) {
+    if (num>1) {
+      return " are ";
+    }
+    else {
+      return " is ";
+    }
+  }
+  if (num>1) {
+    return " " + str + "s";
+  }
+return " " + str;
+}
+  
+  public static int getPlayer(String[][] arr, String which) {
+    for (int i = 0; i < arr.length; i++) {
+      for(int j = 0; j<arr[i].length; j++) 
+      {
+        if(arr[i][j].equals("+") || (arr[i][j].equals("@"))) {
+          if(which.equals("row")) {
+            return i;
+          }
+          else {
+            return j;
+          }
+        }
+      }
+}
+    return -1;
+  }
+
+ 
+  public static String[][] removeLine(String[][] arr) {
+    String[][] returnArr = new String[arr.length-1][arr[1].length];
+    for (int i = 1; i < arr.length; i++) {
+      for(int j = 0; j< arr[i].length; j++) {
+        returnArr[i-1][j] = arr[i][j];
+      }
+}
+    return returnArr;
+  }
+
+   /*private static String readFile(String filePath) {
+       StringBuilder sb = new StringBuilder();
+
+       try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath))) {
+
+           String line = br.readLine();
+           while (line != null) {
+               sb.append(line).append(System.getProperty("line.separator"));
+               line = br.readLine();
+           }
+
+       } catch (Exception errorObj) {
+           System.err.println("Couldn't read file: " + filePath);
+           errorObj.printStackTrace();
+       }
+
+       return sb.toString();
+   }
+
+   public static void writeDataToFile(String filePath, String data) throws IOException {
+       try (FileWriter f = new FileWriter(filePath);
+            BufferedWriter b = new BufferedWriter(f);
+            PrintWriter writer = new PrintWriter(b);) {
+
+
+           writer.println(data);
+
+
+       } catch (IOException error) {
+           System.err.println("There was a problem writing to the file: " + filePath);
+           error.printStackTrace();
+       }
+   }*/
+}
---
+++ src/main/java/State.java
@@ -0,0 +1,230 @@
+import java.util.ArrayList;
+
+public class State {
+   /* attributes to represent current state */
+   private State parent;                // ← reference to parent.  Null if no parent.
+   public String[][] board;
+  public int stepsNeeded;
+  public int pR;
+  public int pC;
+  String[][] savedBoard;
+  int savedpR;
+  int savedpC;
+  int counter; 
+
+   public State(State parent, String[][] initBoard, int stepsNeeded, int r, int c) {
+       this.parent = parent;
+     this.board = initBoard;
+     this.savedBoard = initBoard;
+     System.out.println("run");
+     this.stepsNeeded = stepsNeeded;
+     this.pR = r;
+     this.pC = c;
+    this.savedBoard = board;
+    this.savedpR = r;
+    this.savedpC = c;
+     
+   }
+
+   public void setParent(State p) {
+       this.parent = p;
+   }
+
+   public boolean isGoal() {          
+     // return true if this state is the goal
+
+     for(int i = 0; i<board.length; i++) {
+       for(int j = 0; j<board[i].length; j++) {
+         if(board[i][j].equals(".") || board[i][j].equals("+")) return false;
+       }
+     }
+     
+       return true;
+   }
+
+   public ArrayList<State> getNextStates() { 
+     // return list of next states from current
+       return null;                // ** be careful: return COPIES **
+       // ** remember to set the parent of each copy to this
+//    object **
+   }
+
+   public boolean equals(State other) { 
+     // return true if current state is same as other
+       return false;
+   }
+
+   public State copy() {
+     // return a copy of the current state
+     State copy = new State(this.parent, this.board, this.stepsNeeded, this.pR, this.pC);
+       return copy;              // ** be careful:  you must run the constructor
+       //  here!! Don’t return a reference to this **
+   }
+
+   public String toString() {            // what the current state should look like when
+       // printed
+       return "";
+   }
+
+
+   // public ArrayList<Move> getNextMoves() {        // return list of valid moves
+   //     return null;
+   // }
+
+  public  void print(String[][] arr) {
+    for(int i = 0; i<arr.length; i++) {
+      for(int j = 0; j<arr[i].length; j++) {
+        System.out.print(arr[i][j]);
+      }
+      System.out.println();
+    }
+  }
+
+
+   public void applyMove(String move) { 
+     // apply move m to current state
+     if(move.equals("reset")) {
+       this.board = this.savedBoard;
+       print(this.savedBoard);
+       pR = savedpR;
+       pC = savedpC;
+       counter = 0;
+       return;
+     }
+     
+     if(this.isValidMove(move)){
+     if(move.equals("l")) {
+       if( board[pR][pC-1].equals("$") ||  board[pR][pC-1].equals("*")) {
+         if(board[pR][pC-2].equals(".")) {
+           board[pR][pC-2] = "*";
+         }else {
+           board[pR][pC-2] = "$";
+         }
+       }
+       if(board[pR][pC-1].equals(".")) {
+         board[pR][pC-1] = "+";
+       } else {
+         board[pR][pC-1] = "@";
+       }
+       if(board[pR][pC].equals("+")) {
+         board[pR][pC] = ".";
+       } else {
+       board[pR][pC] = " ";
+       }
+       pC--;
+     }
+     if(move.equals("r")) {
+       if( board[pR][pC+1].equals("$") ||  board[pR][pC+1].equals("*")) {
+         if(board[pR][pC+2].equals(".")) {
+           board[pR][pC+2] = "*";
+         }else {
+           board[pR][pC+2] = "$";
+         }
+       }
+        if(board[pR][pC+1].equals(".")) {
+          board[pR][pC+1] = "+";
+        } else {
+          board[pR][pC+1] = "@";
+        }
+        if(board[pR][pC].equals("+")) {
+          board[pR][pC] = ".";
+        } else {
+        board[pR][pC] = " ";
+        }
+        pC++;
+      }
+     if(move.equals("u")) {
+        if( board[pR-1][pC].equals("$") ||  board[pR-1][pC].equals("*")) {
+          if(board[pR-2][pC].equals(".")) {
+            board[pR-2][pC] = "*";
+          }else {
+            board[pR-2][pC] = "$";
+          }
+        }
+        if(board[pR-1][pC].equals(".")) {
+          board[pR-1][pC] = "+";
+        } else {
+          board[pR-1][pC] = "@";
+        }
+        if(board[pR][pC].equals("+")) {
+          board[pR][pC] = ".";
+        } else {
+        board[pR][pC] = " ";
+        }
+        pR--;
+      }
+     if(move.equals("d")) {
+       if( board[pR+1][pC].equals("$") ||  board[pR+1][pC].equals("*")) {
+         if(board[pR+2][pC].equals(".")) {
+           board[pR+2][pC] = "*";
+         }else {
+           board[pR+2][pC] = "$";
+         }
+       }
+       if(board[pR+1][pC].equals(".")) {
+         board[pR+1][pC] = "+";
+       } else {
+         board[pR+1][pC] = "@";
+       }
+       if(board[pR][pC].equals("+")) {
+         board[pR][pC] = ".";
+       } else {
+       board[pR][pC] = " ";
+       }
+       pR++;
+     }
+
+   counter++;
+     }}
+
+  /*
+  u = up;
+  l = left;
+  d = down;
+  r = right;
+  */
+
+   public boolean isValidMove(String move) { 
+     // return true if move is valid
+     if(move.equals("l")) {
+       if(board[pR][pC-1].equals("$") || board[pR][pC-1].equals("*")) {
+         if(board[pR][pC-2].equals("#") || 
+            board[pR][pC-2].equals("$") || board[pR][pC-2].equals("*")) {
+           return false;
+         }
+       }
+       if(board[pR][pC-1].equals("#")) return false;
+     }
+     if(move.equals("r")) {
+        if(board[pR][pC+1].equals("$") || board[pR][pC+1].equals("*")) {
+          if(board[pR][pC+2].equals("#") || 
+             board[pR][pC+2].equals("$") || board[pR][pC+2].equals("*")) {
+            return false;
+          }
+        }
+        if(board[pR][pC+1].equals("#")) return false;
+      }
+     if(move.equals("u")) {
+        if(board[pR-1][pC].equals("$") || board[pR-1][pC].equals("*")) {
+          if(board[pR-2][pC].equals("#") || 
+             board[pR-2][pC].equals("$") || board[pR-2][pC].equals("*")) {
+            return false;
+          }
+        }
+        if(board[pR-1][pC].equals("#")) return false;
+      }
+     if(move.equals("d")) {
+       if(board[pR+1][pC].equals("$") || board[pR+1][pC].equals("*")) {
+         if(board[pR+2][pC].equals("#") || 
+            board[pR+2][pC].equals("$") || board[pR+2][pC].equals("*")) {
+           return false;
+         }
+       }
+       if(board[pR+1][pC].equals("#")) return false;
+     }
+       return true;
+   }
+
+}
+
+
